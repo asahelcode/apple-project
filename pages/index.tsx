64a6cb7d3d8626247a5b045c7756b74bef5b0e2e
asahelcode <hoyx0101@gmail.com>
2022-10-10 10:01:02 +0100

    Next app with tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h2>
+        Building Apple design
+      </h2>
+    </div>
+  )
+}
+
+export default Home
